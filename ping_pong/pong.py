295a70f25183cce369301a38beabf4c08aaf63d4
Adijat Ojutomori <aojutomori@gmail.com>
2025-09-04 14:14:34 +0000

    log app and pond shared persistent stoage

--- ping_pong/pong.py
+++ ping_pong/pong.py
@@ -4,12 +4,17 @@ app = Flask(__name__)
 
 # In-memory counter (will reset when app restarts)
 counter = 0
+LOG_FILE = '/usr/src/app/files/data.log'
+# LOG_FILE = 'data.log'
 
 @app.route('/pingpong', methods=['GET'])
 def pingpong():
     global counter
     response = f"pong {counter}"
+    line = f"Ping / Pongs: {counter}\n"
     counter += 1
+    with open(LOG_FILE, 'a') as f:
+        f.write(line)
     return response
 
 if __name__ == '__main__':
